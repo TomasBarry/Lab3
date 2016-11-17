64d9f8975f0d05fda5183e151da7374290fe655f
Tomas Barry <tomas.barry@hotmail.com>
2016-11-17 11:46:00 +0000

    Prepatory code to handle multiple clients

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
-var net = require('net');
-var handler = require('./request-handler.js');
-var logger = require('./logger.js');
+// Import node socket module, command handler and custom logger
+const net = require('net');
+const handler = require('./request-handler.js');
+const logger = require('./logger.js');
+const util = require('util');
 
+// used to keep track of connected clients 
+var clients = {};
+
+// create the Node server and wait for socket connections
 var server = net.createServer((socket) => {
 	logger.log('info', 'New socket connection');
+	
+	// store the client socket
+	socket.key = socket.remoteAddress + ":" + socket.remotePort;
+	clients[socket.key] = socket;
+
+	// Event for when data is read from a socket connection
 	socket.on('data', (data) => {
-		logger.log('info', data.toString());
+		logger.log('info', 'Socket(' + socket.key + ') data: ' + data.toString());
 		handler.handleData(data.toString());
+	});
+
+	// Event for when the client disconnects
+	socket.on('close', () => {
+		delete clients[socket.key];
+		logger.log('info', 'Deleted client. Clients: ' + Object.keys(clients).length);	
 	});	
 });
 
@@ -14,7 +32,8 @@ server.on('error', (err) => {
 	// handle errors here
 	logger.log('err', 'server error');
 });
+
 // grab a random port.
 server.listen(8000, () => {
-	logger.log('info', 'opened server on ' + server.address());
+	logger.log('info', 'opened server on ' + util.inspect(server.address()));
 });
