bebd33fff0e20ed6f25d5f58a46de819124bf978
Tomas Barry <tomas.barry@hotmail.com>
2016-11-17 11:55:52 +0000

    Added in basic handlers for valid commands

--- request-handler.js
+++ request-handler.js
@@ -1,10 +1,23 @@
+// Import the chatroom object and custom logger
 var chatrooms = require('./chatrooms-json.js');
-var logger = require('./logger.js');
+const logger = require('./logger.js');
 
+
+// This object will be exported. It handles all incoming requests to the server
 const handler = {
-	handleData: function(data) {
+
+	handleData: function(socket, data) {
 	if (data.indexOf('JOIN_CHATROOM') === 0) {
-		logger.log('info', 'Joining a chatroom');
+		logger.log('info', socket.key + ' is attempting to join a chatroom');
+	}
+	else if (data.indexOf('LEAVE_CHATROOM') === 0) {
+		logger.log('info', socket.key + ' is attempting to leave a chatroom');
+	}
+	else if(data.indexOf('DISCONNECT') === 0) {
+		logger.log('info', socket.key + ' is attempting to disconnect');
+	}
+	else if(data.indexOf('CHAT') === 0) {
+		logger.log('info', socket.key + ' is attempting to chat');
 	}
 	else {
 		logger.log('warn', 'Unknown command');
