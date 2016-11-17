7fa4e9bbd77cc735c3988da49d9a346322d42d36
Tomas Barry <tomas.barry@hotmail.com>
2016-11-17 14:46:52 +0000

    Handle requests from lab 2

--- request-handler.js
+++ request-handler.js
@@ -41,42 +41,52 @@ const handler = {
 	},
 
 	handleData: function(socket, data) {
-	/** 
-	 * Request:
-	 * JOIN_CHATROOM: [chatroom name]
-	 * CLIENT_IP: [0]
-	 * PORT: [0]
-	 * CLIENT_NAME: [string Handle to identifier client user]
-	 * Response:
-	 * JOINED_CHATROOM: [chatroom name]
-	 * SERVER_IP: [server IP]
-	 * PORT: [server port]
-	 * ROOM_REF: [integer that uniquely identifies chat room on server]
-	 * JOIN_ID: [integer that uniquely identifies client joining]
-	 */
-	if (data.indexOf('JOIN_CHATROOM') === 0) {
-		logger.log('info', socket.key + ' is attempting to join a chatroom');
-		let requestLines = data.split('\n');
-		logger.log('info', requestLines[0]);
-		logger.log('info', requestLines[3]);
-		let chatroomName = parseValue(requestLines[0]);
-		let clientName = parseValue(requestLines[3]);
-		joinChatroom(chatroomName, clientName);	
-	}
+		let responseText = '';
+		/** 
+		 * Request:
+		 * JOIN_CHATROOM: [chatroom name]
+		 * CLIENT_IP: [0]
+		 * PORT: [0]
+		 * CLIENT_NAME: [string Handle to identifier client user]
+		 * Response:
+		 * JOINED_CHATROOM: [chatroom name]
+		 * SERVER_IP: [server IP]
+		 * PORT: [server port]
+		 * ROOM_REF: [integer that uniquely identifies chat room on server]
+		 * JOIN_ID: [integer that uniquely identifies client joining]
+		 */
+		if (data.indexOf('JOIN_CHATROOM') === 0) {
+			logger.log('info', socket.key + ' is attempting to join a chatroom');
+			let requestLines = data.split('\n');
+			logger.log('info', requestLines[0]);
+			logger.log('info', requestLines[3]);
+			let chatroomName = parseValue(requestLines[0]);
+			let clientName = parseValue(requestLines[3]);
+			joinChatroom(chatroomName, clientName);	
+		}
 		
-	else if (data.indexOf('LEAVE_CHATROOM') === 0) {
-		logger.log('info', socket.key + ' is attempting to leave a chatroom');
+		else if (data.indexOf('LEAVE_CHATROOM') === 0) {
+			logger.log('info', socket.key + ' is attempting to leave a chatroom');
+		}
+		else if (data.indexOf('DISCONNECT') === 0) {
+			logger.log('info', socket.key + ' is attempting to disconnect');
+		}
+		else if (data.indexOf('CHAT') === 0) {
+			logger.log('info', socket.key + ' is attempting to chat');
+		}
+		else if (data.indexOf('HELO') === 0) {
+			logger.log('info', socket.key + ' is attempting to say HELO');   
+			responseText = data + 'IP:' + serverAddress + '\nPort:' + serverPort + '\nStudentID:13321218\n';
+			socket.write(responseText);
+		}
+		else if (data.indexOf('KILL_SERVICE') === 0) {
+			logger.log('info', socket.key + ' is attempting to kill me');   
+			process.exit();
+		}
+		else {
+			logger.log('warn', 'Unknown command');
+		}
 	}
-	else if(data.indexOf('DISCONNECT') === 0) {
-		logger.log('info', socket.key + ' is attempting to disconnect');
-	}
-	else if(data.indexOf('CHAT') === 0) {
-		logger.log('info', socket.key + ' is attempting to chat');
-	}
-	else {
-		logger.log('warn', 'Unknown command');
-	}
-    }
 };
 
 module.exports = handler;
